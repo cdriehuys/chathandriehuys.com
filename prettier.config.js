7bb215a1b3bccbe3e63b3118e30e032002b438f1
Chathan Driehuys <chathan@driehuys.com>
2023-04-26 15:16:27 +0000

    feat!: Major rewrite
    Removes all content since I'm not much of a blogger. Keeps the landing
    page so I have some sort of internet presence.

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  overrides: [
+    {
+      files: ["*.html"],
+      options: {
+        parser: "go-template",
+      },
+    },
+  ],
+  plugins: [
+    require("prettier-plugin-go-template"),
+    require("prettier-plugin-tailwindcss"),
+  ],
+};
