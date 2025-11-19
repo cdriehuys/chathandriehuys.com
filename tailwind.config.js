1df8fcd66546dbedc2ef9d8ee3d0151e10883f01
Chathan Driehuys <chathan@driehuys.com>
2025-11-19 07:39:42 -0500

    chore: update to Hugo v0.152.2 (#48)
    This allows for simplifying the build process because of the new
    features included in Hugo.

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["content/**/*.md", "data/**/*.yml", "layouts/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
